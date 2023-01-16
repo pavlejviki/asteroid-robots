fb7a597ad2809e4af640e05472a931912d284584
Viktoria Pavlej <viktorialondon16@gmail.com>
2023-01-16 09:52:13 +0000

    Add test for initializing Robot class

---
+++ tests/test_robot.py
@@ -0,0 +1,11 @@
+from robot import Robot
+
+
+def test_robot_created_with_correct_initial_state(robot):
+    robot = Robot(2, 2, "north", 5, 5)
+    assert robot.x == 2
+    assert robot.y == 2
+    assert robot.bearing == "north"
+    assert robot.x_boundary == 5
+    assert robot.y_boundary == 5
+    assert isinstance(robot, Robot)
