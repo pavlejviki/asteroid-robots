7b017dc1d95a08527841e907b7f4dc0123d35d6c
Viktoria Pavlej <viktorialondon16@gmail.com>
2023-01-16 12:37:13 +0000

    Implement init method for Asteroid class

--- asteroid.py
+++ asteroid.py
@@ -1,3 +1,5 @@
 class Asteroid:
-    def __init__(self):
-        pass
+    def __init__(self, size_x: int, size_y:int) -> None:
+        self.size_x = size_x
+        self.size_y = size_y
+        self.robots = []
