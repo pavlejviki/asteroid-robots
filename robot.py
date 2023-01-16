faad25b482d5f6346ca6652f6f2114e1748c3c35
Viktoria Pavlej <viktorialondon16@gmail.com>
2023-01-16 09:54:20 +0000

    Implement init method for Robot class

--- robot.py
+++ robot.py
@@ -1,3 +1,9 @@
 class Robot:
-    def __init__(self):
-        pass
+    def __init__(
+            self, x: int, y: int, bearing: str, x_boundary: int, y_boundary: int
+    ) -> None:
+        self.x = x
+        self.y = y
+        self.bearing = bearing
+        self.x_boundary = x_boundary
+        self.y_boundary = y_boundary
--- tests/test_robot.py
+++ tests/test_robot.py
@@ -1,7 +1,7 @@
 from robot import Robot
 
 
-def test_robot_created_with_correct_initial_state(robot):
+def test_robot_created_with_correct_initial_state():
     robot = Robot(2, 2, "north", 5, 5)
     assert robot.x == 2
     assert robot.y == 2
