22667666faf2f03e9da428e9353f5fb9a9014d9a
Viktoria Pavlej <viktorialondon16@gmail.com>
2023-01-16 10:18:12 +0000

    Add test for raising ValueError when initializing x,y coordinate outside boundary

--- tests/test_robot.py
+++ tests/test_robot.py
@@ -1,12 +1,11 @@
 import pytest
-
 from robot import Robot
 
 
 @pytest.fixture
 def robot():
     robot = Robot(2, 2, "north", 5, 5)
-    yield robot
+    return robot
 
 
 def test_robot_created_with_correct_initial_state(robot):
@@ -16,3 +15,8 @@ def test_robot_created_with_correct_initial_state(robot):
     assert robot.x_boundary == 5
     assert robot.y_boundary == 5
     assert isinstance(robot, Robot)
+
+
+def test_value_error_raised_when_initializing_x_y_coordinate_outside_boundary():
+    with pytest.raises(ValueError, match="Invalid x coordinate for robot, must be within boundaries"):
+        Robot(6, 2, "north", 5, 5)
