b476f36d58f5fe2a9644ef75203c6c6f832e6cf6
Viktoria Pavlej <viktorialondon16@gmail.com>
2023-01-16 12:48:33 +0000

    Add test for raising ValueError when initializing invalid  x,y size for asteroid

--- tests/test_asteroid.py
+++ tests/test_asteroid.py
@@ -1,6 +1,7 @@
 import pytest
 
 from asteroid import Asteroid
+from robot import Robot, Bearing
 
 
 @pytest.fixture
@@ -8,7 +9,21 @@ def asteroid():
     return Asteroid(5, 5)
 
 
-def test_asteroid_created_with_correct_size_and_without_robots(robot):
+def test_asteroid_created_with_correct_size_and_without_robots(asteroid):
     assert asteroid.size_x == 5
     assert asteroid.size_y == 5
     assert asteroid.robots == []
+
+
+@pytest.mark.parametrize(
+    "x, y, error_message",
+    [
+        (-1, 5, "Invalid x coordinate for the size of the asteroid, can't be negative"),
+        (5, -5, "Invalid y coordinate for the size of the asteroid, can't be negative"),
+    ],
+)
+def test_value_error_raised_when_initializing_invalid_x_y_size_for_asteroid(
+    x, y, error_message
+):
+    with pytest.raises(ValueError, match=error_message):
+        Asteroid(x, y)
